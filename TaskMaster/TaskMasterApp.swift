48d445f477ea46299e7083b792cd8aaa453fdf49
liaozijian <lzj960515@163.com>
2025-02-26 10:16:24 +0800

    Initial Commit

---
+++ TaskMaster/TaskMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterApp.swift
+//  TaskMaster
+//
+//  Created by liaozijian on 2025/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskMasterTests/TaskMasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterTests.swift
+//  TaskMasterTests
+//
+//  Created by liaozijian on 2025/2/26.
+//
+
+import Testing
+@testable import TaskMaster
+
+struct TaskMasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
